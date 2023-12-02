adffdce2682a7543a2b26be270532ebf2d83b856
Rohith-Manjunath <rohithmanjunath20@gmail.com>
2023-12-03 00:41:44 +0530

    Carousel fix

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,9 +1,8 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Image1 from "../assets/images/chairs.jpg";
-import Image2 from "../assets/images/football.jpg";
-import Image3 from "../assets/images/library.jpg";
+
+import { carouselData } from "../data/carouselData";
 
 const Carousel = () => {
   let slickSlider;
@@ -44,69 +43,31 @@ const Carousel = () => {
         prevArrow={<CustomPrevArrow />}
         nextArrow={<CustomNextArrow />}
       >
-        <div className="slide">
-          <div
-            className="w-full h-[30rem] bg-cover bg-center flex items-center justify-start p-[2rem] md:p-[4rem]"
-            style={{
-              backgroundImage: `url(${Image3})`,
-            }}
-          >
-            <div className="text-white text-left flex flex-col gap-3 w-full md:w-[60%] h-auto ml-0 md:ml-[2rem] bg-[rgba(211,210,210,0.1)] p-6 md:p-10 rounded-lg">
-              <h1 className="text-2xl md:text-4xl font-bold text-primary">ABOUT US</h1>
-              <p className="font-bold tracking-[1px] text-sm md:text-base text-primary">
-                At Mysore International School, we uniquely shape futures
-                through academic excellence and holistic development. Join us
-                for a transformative journey, empowering students for a
-                confident and impactful future.
-              </p>
+        {carouselData.map((item) => {
+          return (
+            <div className="slide" key={item.id}>
+              <div
+                className="w-full h-[30rem] bg-cover bg-center flex items-center justify-start p-[2rem] md:p-[4rem]"
+                style={{
+                  backgroundImage: `url(${item.image})`,
+                }}
+              >
+                <div className="text-white text-left flex flex-col gap-3 w-full md:w-[60%] h-auto ml-0 md:ml-[2rem] bg-[rgba(211,210,210,0.1)] p-6 md:p-10 rounded-lg">
+                  <h1 className="text-2xl md:text-4xl font-bold text-primary">
+                    {item.title}
+                  </h1>
+                  <p className="font-bold tracking-[1px] text-sm md:text-base text-primary">
+                    {item.description}
+                  </p>
 
-              <button className="btn p-2 rounded-md font-semibold w-[30%] md:w-[20%] bg-[#01416f] text-sm md:text-base">
-                Know More
-              </button>
+                  <button className="btn p-2 rounded-md font-semibold w-[30%] md:w-[20%] bg-[#01416f] text-sm md:text-base">
+                    Know More
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="slide">
-          <div
-            className="w-full h-[30rem] bg-cover bg-center flex items-center justify-start p-[2rem] md:p-[4rem]"
-            style={{
-              backgroundImage: `url(${Image2})`,
-            }}
-          >
-            <div className="text-white text-left flex flex-col gap-3 w-full md:w-[60%] h-auto ml-0 md:ml-[2rem] bg-[rgba(211,210,210,0.3)] p-6 md:p-10 rounded-lg">
-              <h1 className="text-2xl md:text-4xl font-bold text-primary">OUR TEAM</h1>
-              <p className="font-bold tracking-[1px] text-sm md:text-base text-primary">
-                At Mysore International School, our committed team nurtures
-                academic excellence and personal growth. Meet the passionate
-                educators shaping confident global citizens.
-              </p>
-              <button className="btn p-2 rounded-md font-semibold w-[30%] md:w-[20%] bg-[#01416f] text-sm md:text-base">
-                Know More
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="slide">
-          <div
-            className="w-full h-[30rem] bg-cover bg-center flex items-center justify-start p-[2rem] md:p-[4rem]"
-            style={{
-              backgroundImage: `url(${Image1})`,
-            }}
-          >
-            <div className="text-white text-left flex flex-col gap-3 w-full md:w-[60%] h-auto ml-0 md:ml-[2rem] bg-[rgba(211,210,210,0.1)] p-6 md:p-10 rounded-lg ">
-              <h1 className="text-2xl md:text-4xl font-bold text-primary">WE BELIEVE</h1>
-              <p className="font-bold tracking-[1px] text-sm md:text-base text-primary">
-                At Mysore International School, we believe in the transformative
-                power of education. Our commitment is to nurture each student's
-                potential, providing a foundation for a brighter, empowered
-                future.
-              </p>
-              <button className="btn p-2 rounded-md font-semibold w-[30%] md:w-[20%] bg-[#01416f] text-sm md:text-base">
-                Know More
-              </button>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </Slider>
     </div>
   );
---
+++ client/src/data/carouselData.jsx
@@ -0,0 +1,32 @@
+import Image1 from "../assets/images/chairs.jpg";
+import Image2 from "../assets/images/football.jpg";
+import Image3 from "../assets/images/library.jpg";
+
+export const carouselData = [
+  {
+    id: 1,
+    title: "ABOUT US",
+    description: `At Mysore International School, we uniquely shape futures
+      through academic excellence and holistic development. Join us
+      for a transformative journey, empowering students for a
+      confident and impactful future.`,
+    image: Image1,
+  },
+  {
+    id: 2,
+    title: "OUR TEAM",
+    description: `At Mysore International School, our committed team nurtures
+      academic excellence and personal growth. Meet the passionate
+      educators shaping confident global citizens.`,
+    image: Image2,
+  },
+  {
+    id: 3,
+    title: "WE BELIEVE",
+    description: `At Mysore International School, we believe in the transformative
+      power of education. Our commitment is to nurture each student's
+      potential, providing a foundation for a brighter, empowered
+      future.`,
+    image: Image3,
+  },
+];
