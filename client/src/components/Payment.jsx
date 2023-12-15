95f609303c352c76cceb9b943d61baacee0a0c89
Bibin Antony <40894472+Bibin-Antony@users.noreply.github.com>
2023-12-15 23:21:54 +0530

    update

--- client/src/components/Payment.jsx
+++ client/src/components/Payment.jsx
@@ -1,21 +1,17 @@
-import Image from "../assets/images/PaymentBG.png";
+import Image from '../assets/Payment/background/payment_bg.png';
 
 const Payment = () => {
   return (
     <div className="p-20 border border-b-[#8a2d87] bg-[#580b57] text-white">
       <div className="flex p-2 ">
         <div className="w-1/2 flex flex-col items-start gap-10 justify-start">
-          <h2 className="text-5xl font-semibold w-1/2">Online Fee Payment</h2>
-          <p className="font-semibold">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias
-            aperiam iusto ad nostrum inventore reprehenderit voluptas eos
-            pariatur accusantium illum consequuntur sunt, neque veritatis non ea
-            soluta facilis aliquid itaque.
+          <h2 className="text-5xl font-semibold w-{5rem} tracking-wider">Online Fee Payment</h2>
+          <p className="font-semibold tracking-wider">
+          At Mysore International School, we believe in simplifying the school experience for parents. Opting for our secure Online Fees Payments not only streamlines the process but also brings a host of benefits to your fingertips.
           </p>
           <a
-            href="https://www.google.com/maps/@12.2291753,76.5867069,3a,90y,9.72h,90.02t/data=!3m7!1e1!3m5!1sAF1QipMc3j45DwgTUz5qLOpMenxniJsUA6-V2ipfptzO!2e10!3e12!7i6720!8i3360?entry=ttu
-"
-            className=" border w-1/4 bg-white text-center text-[#580b57] font-semibold p-2 rounded-sm hover:scale-105 transition-all duration-300 hover:text-white hover:bg-[#580b57] hover:border hover:border-slate-400"
+            href=""
+            className=" border w-1/4 bg-white text-center text-[#580b57] font-semibold p-2 rounded-sm transition-all duration-300  hover:border hover:border-slate-400 hover:tracking-widest hover:scale-105"
             target="_blank"
             rel="noopener noreferrer"
           >
--- client/src/data/ReasonsToChoose.jsx
+++ client/src/data/ReasonsToChoose.jsx
@@ -1,24 +1,24 @@
-import Image1 from "../assets/Reason/like.png";
+import { GrSecure } from "react-icons/gr";
 import Image2 from "../assets/Reason/credit-card.png";
 import Image3 from "../assets/Reason/track.png";
 
 export const ReasonToChoose = [
   {
     id: 1,
-    title: "Smart & easy transfer",
-    description: `Avoid wasting time easily transfer money using UPI/Card Payment`,
-    img: Image1,
+    title: "Secure Transactions",
+    description: `Our online platform ensures the confidentiality and security of your payment information.`,
+    img: <GrSecure className="h-[3rem]"/>,
   },
   {
     id: 2,
-    title: "Secure & Compliment",
-    description: `We keep your financial details private and transactions secure from fraud`,
-    img: Image2,
+    title: "Time-Saving",
+    description: `Skip the queues and complete your transactions swiftly, allowing you more time for what matters.`,
+    img: <GrSecure />,
   },
   {
     id: 3,
-    title: "Simple & Trackable",
-    description: `Skip Entering and updating your card details every time you check out`,
-    img: Image3,
+    title: "Convenience",
+    description: `Pay fees anytime, anywhere, without the need to visit the school in person.`,
+    img: <GrSecure />,
   },
 ];
